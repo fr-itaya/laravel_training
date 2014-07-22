20a5d09d52f065aeb6da7e439524fd180ec8eddb
fr-itaya <itaya@fact-real.com>
2014-07-22 06:53:48 +0000

    #42 カスタムバリデーション：全角正規表現の第3引数削除

--- app/validators/customValidation.php
+++ app/validators/customValidation.php
@@ -2,7 +2,7 @@
 
 class CustomValidator
 {
-    public function regexFullWidthChars ($attribute, $value, $param = null)
+    public function regexFullWidthChars ($attribute, $value)
     {
         if (preg_match("/(?:\xEF\xBD[\xA1-\xBF]|\xEF\xBE[\x80-\x9F])|[\x20-\x7E]/", $value)) {
             return false;
