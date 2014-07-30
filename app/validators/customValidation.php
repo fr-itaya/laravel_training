4dcc8ffcd2fc63d3787fd7baaead5eeba6fb80f0
fr-itaya <itaya@fact-real.com>
2014-07-30 07:54:44 +0000

    #42 カスタムバリデーションの条件分岐修正

--- app/validators/customValidation.php
+++ app/validators/customValidation.php
@@ -5,8 +5,9 @@ class CustomValidator
     public function regexFullWidthChars ($attribute, $value)
     {
         $pattern = "/(?:\xEF\xBD[\xA0-\xBF]|\xEF\xBE[\x80-\x9F])|[\x20-\x7E]/";
-        if (!preg_match($pattern, $value)) {
-            return true;
+        if (preg_match($pattern, $value)) {
+            return false;
         }
+        return true;
     }
 }
