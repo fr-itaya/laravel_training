466544e5868f0f64c8e46f9a25493f1f7f7eb281
fr-itaya <itaya@fact-real.com>
2014-09-16 05:00:32 +0000

    #44 空白トリム処理メソッド化

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -16,18 +16,7 @@ class FormController extends BaseController {
     public function postConfirm() {
         Input::flash();
         if ($form_data = Input::all()) {
-            $form_data_trimmed = array();
-            foreach ($form_data as $key => $val) {
-                if (is_array($val)) {
-                    //配列の場合
-                    foreach ($val as $key_array => $val_array) {
-                        $form_data_trimmed[$key][$key_array] = trim(mb_convert_kana($val[$key_array], 's', 'utf-8'));
-                    }
-                } else {
-                    //変数の場合
-                    $form_data_trimmed[$key] = trim(mb_convert_kana($val, 's', 'utf-8'));
-                }
-            }
+            $form_data_trimmed = FormController::trimSpaces($form_data);
 
             //入力値バリデート
             Validator::extend('regex_full_width_chars', 'CustomValidator@regexFullWidthChars');
@@ -66,7 +55,6 @@ class FormController extends BaseController {
                 'hobby.4'             => 'その他の詳細'
             );
 
-
             FormController::hobbyAutoCheck();
 
             $validator = Validator::make($form_data_trimmed, $rules, $error_messages);
@@ -88,6 +76,24 @@ class FormController extends BaseController {
         return View::make('confirm')->with(array('hobby_view' => $hobby_view, 'pref_view' => $pref_view));
     }
 
+    public function trimSpaces($form_data) {
+        $form_data_trimmed = array();
+        foreach ($form_data as $key => $val) {
+            if (is_array($val)) {
+                //配列の場合
+                foreach ($val as $key_array => $val_array) {
+                    $form_data_trimmed[$key][$key_array] = trim(mb_convert_kana($val[$key_array], 's', 'utf-8'));
+                }
+            } else {
+                //変数の場合
+                $form_data_trimmed[$key] = trim(mb_convert_kana($val, 's', 'utf-8'));
+            }
+        }
+        Input::merge($form_data_trimmed);
+        Input::flash();
+        return $form_data_trimmed;
+    }
+
     public function hobbyAutoCheck() {
         //チェックボックスへの自動入力
         $hobbies = array(
@@ -96,7 +102,7 @@ class FormController extends BaseController {
             3 => "その他：",
             4 => Input::get('hobby.4')
         );
-        if (Input::has('hobby.4') && empty(Input::get('hobby.3'))) {
+        if (!empty(Input::get('hobby.4')) && empty(Input::get('hobby.3'))) {
             Input::merge(array('hobby' => $hobbies));
             Input::flash();
         }
