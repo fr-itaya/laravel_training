2284624af46cee5d152cad7328d0089d7e9c4302
fr-itaya <itaya@fact-real.com>
2014-10-02 10:41:24 +0000

    #44 ModelをValidateに移動・Controllerが動かなくなっていたので修正

---
+++ app/tests/models/ValidationTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class ValidationTest extends TestCase
+{
+    public function testIsInvaild() {
+
+    }
+
+}
--- app/validators/customValidation.php
+++ app/validators/customValidation.php
@@ -1,13 +1,9 @@
 <?php
 
-class CustomValidator
-{
-    public function regexFullWidthChars ($attribute, $value)
-    {
+class CustomValidator {
+    public function regexFullWidthChars ($attribute, $value) {
         $pattern = "/(?:\xEF\xBD[\xA0-\xBF]|\xEF\xBE[\x80-\x9F])|[\x20-\x7E]/";
-        if (preg_match($pattern, $value)) {
-            return false;
-        }
+        if (preg_match($pattern, $value)) return false;
         return true;
     }
 }
