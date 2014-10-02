f9a8a91ed5d83cc8f5b9662deadc52c26b7854ee
fr-itaya <itaya@fact-real.com>
2014-10-02 04:38:41 +0000

    #44 Hobby::hobbyAutoCheck()のリファクタリング4(if/else文を複数のif文に分割)

--- app/models/Hobby.php
+++ app/models/Hobby.php
@@ -3,11 +3,8 @@
 class Hobby {
     public function hobbyAutoCheck($hobbies) {
         //チェックボックスへの自動入力
-        if (empty($hobbies[4])) {
-            return true;
-        } elseif (!empty($hobbies[3])) {
-            return true;
-        }
+        if (empty($hobbies[4]))  return true;
+        if (!empty($hobbies[3])) return true;
         return false;
     }
 }
